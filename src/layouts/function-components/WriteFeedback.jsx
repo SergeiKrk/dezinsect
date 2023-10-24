bd35d2f2401c63d5a46066271db6d0676ea3232c
LAPTOP-MV68K341\ksv <krjukov_s@mail.ru>
2023-10-24 16:48:06 +0400

    test contact

--- src/layouts/function-components/WriteFeedback.jsx
+++ src/layouts/function-components/WriteFeedback.jsx
@@ -1,91 +1,36 @@
-import React, { useState } from "react";
+const fetch = require("node-fetch");
 
-function WriteFeedback() {
-  const [name, setName] = useState("");
-  const [text, setText] = useState("");
-  const [rating, setRating] = useState(1);
+const telegramBotToken = "6735512239:AAEaEX6NrSZWIUaY0z-B-30sJRX78-KKFAo";
+const chatId = "-1002004060031"; // Ваш ID чата в Telegram
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+const express = require("express");
+const app = express();
+const port = 3001; // Порт вашего сервера
 
-    // Отправка данных на сервер или другая обработка отзыва
+app.use(express.json());
 
-    // Очистка полей формы
-    setName("");
-    setText("");
-    setRating(1);
-  };
+app.post("/submit", (req, res) => {
+  const { name, rating, message } = req.body;
 
-  return (
-    <div className="mx-auto mt-4 w-1/2">
-      <form
-        onSubmit={handleSubmit}
-        className="rounded-lg bg-white p-8 shadow-md"
-      >
-        <h2 className="mb-4 text-2xl font-semibold">Оставить отзыв</h2>
-        <div className="mb-4">
-          <label
-            className="mb-2 block text-sm font-medium text-gray-600"
-            htmlFor="name"
-          >
-            Имя:
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full border p-2"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="mb-2 block text-sm font-medium text-gray-600"
-            htmlFor="text"
-          >
-            Текст отзыва:
-          </label>
-          <textarea
-            id="text"
-            name="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            className="w-full border p-2"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="mb-2 block text-sm font-medium text-gray-600"
-            htmlFor="rating"
-          >
-            Оценка:
-          </label>
-          <select
-            id="rating"
-            name="rating"
-            value={rating}
-            onChange={(e) => setRating(e.target.value)}
-            className="border p-2"
-          >
-            {[1, 2, 3, 4, 5].map((num) => (
-              <option key={num} value={num}>
-                {num}
-              </option>
-            ))}
-          </select>
-        </div>
-        <button
-          type="submit"
-          className="rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"
-        >
-          Отправить
-        </button>
-      </form>
-    </div>
-  );
-}
+  if (!name || !rating || !message) {
+    res.status(400).send("Пожалуйста, заполните все поля формы.");
+    return;
+  }
 
-export default WriteFeedback;
+  const text = `Отзыв\nИмя: ${name}\nОценка: ${rating}\nОтзыв: ${message}`;
+
+  const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${chatId}&text=${text}`;
+
+  fetch(url, { method: "POST" })
+    .then(() => {
+      res.status(200).send("Отзыв успешно отправлен в Telegram.");
+    })
+    .catch((error) => {
+      console.error("Ошибка отправки в Telegram:", error);
+      res.status(500).send("Произошла ошибка при отправке отзыва.");
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
